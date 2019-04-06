ef60252fcbaa6d51167e61af6b686047768112e0
mike-petrov <mis5116@yandex.ru>
2019-04-06 18:19:57 +0300

    add chart

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
+// import './function';
+
 
 class App extends React.Component {
     render() {
         return (
-            <div >
-            1
-            </div>
+            <React.Fragment>
+                <div className="general">
+                    <div className="top">
+                        <div className="chart">
+                            <div id="chartdiv"></div>
+                        </div>
+                        <div className="order">
+                            2
+                        </div>
+                    </div>
+                    <div className="bottom">
+                        <div className="content">
+                            3
+                        </div>
+                    </div>
+                </div>
+            </React.Fragment>
         );
     }
 }

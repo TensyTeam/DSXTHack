cf0fa8b7938ee530bf6deebe01a51703319beef3
kosyachniy <polozhev@mail.ru>
2019-04-07 02:01:57 +0300

    Buy & sell

--- Densy.sol
+++ Densy.sol
@@ -3,11 +3,12 @@ contract Densy {
     address private notary;
 
     constructor() public {
-        owner = msg.sender;
-        notary = msg.sender;
+        owner = msg.sender; // Чеканщик - распределение токенов
+        notary = msg.sender; // Нотариус - распределение активов
     }
 
     struct Offer {
+        bool way;
         address owner;
         uint essence;
         uint count;
@@ -15,11 +16,9 @@ contract Densy {
     }
     
     uint private countCoin = 0;
-    mapping (uint => Offer) private offers;
-    mapping (address => uint) private balances;
-    
-    // mapping (uint => uint) private assets;
-    mapping (address => mapping (uint => uint)) private holdings;
+    mapping (uint => Offer) private offers; // Стакан
+    mapping (address => uint) private balances; // Баланс токенов
+    mapping (address => mapping (uint => uint)) private holdings; // Все активы
     
     // Добавление монет
     
@@ -47,29 +46,93 @@ contract Densy {
     }
     
     // Добавить предложение
+    // way: true - продажа, false - покупка
     
-    function addOffer(uint id, uint essence, uint count, uint price) public {
+    function addOffer(bool way, uint id, uint essence, uint count, uint price) public {
         if (holdings[msg.sender][essence] >= count && price > 0) {
             offers[id] = Offer({
+                way: way,
                 owner: msg.sender,
                 essence: essence,
                 count: count,
                 price: price
             });
 
-            holdings[msg.sender][essence] -= count;
+            // Замораживание активов, если на продажу
+            if (way) {
+                holdings[msg.sender][essence] -= count;
+            }
         }
     }
 
     // Схлопывание сделки
+    // idIn - продажа, idOut - покупка
 
-    function swapOffer(uint id) public {
-        uint price = offers[id].price;
-        if (balances[msg.sender] > price) {
-            balances[offers[id].owner] += price;
-            balances[msg.sender] -= price;
-            holdings[msg.sender][offers[id].essence] += offers[id].count;
-            delete offers[id];
+    function swapOffer(uint idIn, uint idOut) public {
+        address ownerIn = offers[idIn].owner;
+        address ownerOut = offers[idOut].owner;
+
+        address access;
+        if (idIn > idOut) {
+            access = ownerIn;
+        } else {
+            access = ownerOut;
+        }
+
+        // Первый продавец?
+        bool cond_in = offers[idIn].way;
+        // Второй покупатель?
+        bool cond_out = !offers[idOut].way;
+        // Вызывает инициатор?
+        bool cond_access = access == msg.sender;
+        // Цена предложения меньше цены покупки?
+        bool cond_price = offers[idIn].price < offers[idOut].price;
+
+        if (cond_in && cond_out && cond_access && cond_price) {
+            uint price;
+            uint count;
+
+            // Определение выгодного для тейкера курса
+
+            if (idIn > idOut) {
+                price = offers[idOut].price;
+            } else {
+                price = offers[idIn].price;
+            }
+            
+            // Случай частичной сделки
+            
+            if (offers[idIn].count < offers[idOut].count) {
+                count = offers[idIn].count;
+            } else {
+                count = offers[idOut].count;
+            }
+            
+            // Итоговая сумма сделки
+            // Объёмы скрыты
+            
+            uint sum = price * count;
+            
+            if (balances[ownerOut] >= sum) {
+                // Изменение балансов
+                balances[ownerIn] += sum;
+                balances[ownerOut] -= sum;
+                
+                // Перечисление активов
+                holdings[ownerOut][offers[idOut].essence] += count;
+
+                // Изменение выполненного количества активов в заявке
+                offers[idIn].count -= count;
+                offers[idOut].count -= count;
+                
+                // Закрытие выполненных заявок
+                if (offers[idIn].count == 0) {
+                    delete offers[idIn];
+                }
+                if (offers[idOut].count == 0) {
+                    delete offers[idOut];
+                }
+            }
         }
     }
 

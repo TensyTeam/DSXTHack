e91ff399ab310c3c0f25571dc7d5b1e6fedc8b8c
mike-petrov <mis5116@yandex.ru>
2019-04-07 13:05:55 +0300

    refactoring and redesign

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -26,6 +26,7 @@ class App extends React.Component {
     onSendOrder() {
         if(this.state.quantity !== null && this.state.price !== null){
     		this.setState({ data: serverResponse(sendOrder(this.state.name, this.state.quantity, this.state.price, this.state.type)) });
+
         }
 	}
 
@@ -99,95 +100,6 @@ class App extends React.Component {
             }
             return chartData;
         }
-
-        //chart 2
-        var data = [];
-        var n1 = this.state.arrayOrders[1].length;
-        var n2 = this.state.arrayOrders[2].length;
-
-        for (let i = 0; i < n1; i++) {
-          let price = Number(this.state.arrayOrders[1][i].price);
-          let quantity = Number(this.state.arrayOrders[1][i].quantity);
-          if(data.length > 0 && price == data[data.length-1].category) {
-              data[data.length-1].value += quantity
-          } else
-            data.push({ value: quantity, category: price });
-        }
-
-        var j = data.length-1;
-        n1 = data.length;
-
-        for (let i = 0; i < n2; i++) {
-          let value = Number(this.state.arrayOrders[2][i].price);
-          let qua = Number(this.state.arrayOrders[2][i].quantity);
-
-          while(j >= 0 && value >= data[j].category){
-            j--;
-          }
-          if(j < 0)
-            j = 0;
-          while(j < n1 && value <= data[j].category){
-            j++;
-          }
-          if(j > 0 && data[j-1].category == value){
-              data.push({ value: data[j-1].value - qua, category: value });
-          }
-          else{
-              data.push({ value: -qua, category: value });
-          }
-        }
-
-        let interfaceColors = new am4core.InterfaceColorSet();
-
-        let chart2 = am4core.create("chartdiv2", am4charts.XYChart);
-
-        chart2.data = data;
-        chart2.bottomAxesContainer.layout = "horizontal";
-        chart2.bottomAxesContainer.reverseOrder = true;
-
-        let categoryAxis = chart2.yAxes.push(new am4charts.CategoryAxis());
-        categoryAxis.dataFields.category = "category";
-        categoryAxis.renderer.grid.template.stroke = interfaceColors.getFor("background");
-        categoryAxis.renderer.grid.template.strokeOpacity = 1;
-        categoryAxis.renderer.grid.template.location = 1;
-        categoryAxis.renderer.minGridDistance = 20;
-
-        let valueAxis2 = chart2.xAxes.push(new am4charts.ValueAxis());
-        valueAxis2.tooltip.disabled = true;
-        valueAxis2.renderer.baseGrid.disabled = true;
-        valueAxis2.marginRight = 3;
-        valueAxis2.renderer.gridContainer.background.fill = interfaceColors.getFor("alternativeBackground");
-        valueAxis2.renderer.gridContainer.background.fillOpacity = 0.05;
-        valueAxis2.renderer.grid.template.stroke = interfaceColors.getFor("background");
-        valueAxis2.renderer.grid.template.strokeOpacity = 1;
-
-        chart2.colors.list = [
-            am4core.color("#000")
-        ];
-
-
-        let series2 = chart2.series.push(new am4charts.ColumnSeries());
-        series2.dataFields.categoryY = "category";
-        series2.dataFields.valueX = "value";
-        series2.xAxis = valueAxis2;
-        series2.name = "Series";
-        series2.tooltipText = "Price: {valueX.value}";
-
-        // series2.stroke = am4core.color("#2ecc71");
-        // series2.fill = am4core.color("#2ecc71");
-
-        // series2.heatRules.push({
-        //  "target": series2.columns.template,
-        //  "property": "fill",
-        //  "min": am4core.color("#000"),
-        //  "max": am4core.color("#fff"),
-        //  "dataField": "valueX"
-        // });
-
-        let bullet = series2.bullets.push(new am4charts.CircleBullet());
-        bullet.fillOpacity = 0;
-        bullet.strokeOpacity = 0;
-        bullet.tooltipText = "{valueX.value}";
     }
 
     render() {
@@ -196,28 +108,48 @@ class App extends React.Component {
                 <Ethreum />
                 <div className="general">
                     <div className="top">
+                        <div className="form-order">
+                            <div className="form">
+                                <select className="input" type="text" defaultValue="usd" name="name" placeholder="Наименование" onChange={(e)=>{this.setState({ name: e.target.value })}}>
+                                    <option value="usd">USD</option>
+                                    <option value="eur">EUR</option>
+                                    <option value="btc">BTC</option>
+                                </select>
+                                <input className="input" type="number" name="quantity" placeholder="Quantity" onChange={(e)=>{this.setState({ quantity: e.target.value })}} required />
+                                <input className="input" type="number" name="price" placeholder="Price" onChange={(e)=>{this.setState({ price: e.target.value })}} required />
+                                <select className="input" type="text" defaultValue="buy" name="type" placeholder="Тип" onChange={(e)=>{this.setState({ type: e.target.value })}}>
+                                    <option value="buy">Buy</option>
+                                    <option value="sell">Sell</option>
+                                </select>
+                                <button className="btn" onClick={()=>{this.onSendOrder()}}>Send</button>
+                            </div>
+                        </div>
                         <div className="chart">
                             <div id="chartdiv1"></div>
                         </div>
                         <div className="order">
-                            <div id="chartdiv2"></div>
-                        </div>
-                    </div>
-                    <div className="title gradient gradient_cold">{this.state.data !== null ? 'Оффер номер: ' + this.state.data : 'Buy / sell'}</div>
-                    <div className="bottom">
-                        <div className="form">
-                            <select className="input" type="text" defaultValue="usd" name="name" placeholder="Наименование" onChange={(e)=>{this.setState({ name: e.target.value })}}>
-                                <option value="usd">Доллар</option>
-                                <option value="eur">Евро</option>
-                                <option value="btc">Биткоин</option>
-                            </select>
-                            <input className="input" type="number" name="quantity" placeholder="Количество" onChange={(e)=>{this.setState({ quantity: e.target.value })}} required />
-                            <input className="input" type="number" name="price" placeholder="Цена" onChange={(e)=>{this.setState({ price: e.target.value })}} required />
-                            <select className="input" type="text" defaultValue="buy" name="type" placeholder="Тип" onChange={(e)=>{this.setState({ type: e.target.value })}}>
-                                <option value="buy">Купить</option>
-                                <option value="sell">Продать</option>
-                            </select>
-                            <button className="btn" onClick={()=>{this.onSendOrder()}}>Отправить</button>
+                            <table>
+                                <thead>
+                                    <tr className="legenda">
+                                        <td>Price</td>
+                                        <td>Quantity</td>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {this.state.arrayOrders[1].map(item =>
+                                        <tr className="sell" key={item.id}>
+                                            <td>{item.price}</td>
+                                            <td>{item.quantity}</td>
+                                        </tr>
+                                    )}
+                                    {this.state.arrayOrders[2].map(item =>
+                                        <tr className="buy" key={item.id}>
+                                            <td>{item.price}</td>
+                                            <td>{item.quantity}</td>
+                                        </tr>
+                                    )}
+                                </tbody>
+                           </table>
                         </div>
                     </div>
                 </div>
